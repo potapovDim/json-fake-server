8d76af19603a65af34913bf460ca131efdf53bc6
potapovDim <potapovd3012@gmail.com>
2017-08-29 09:41:04 +0300

    refactor fake server

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 ":" //; exec /usr/bin/env node --harmony --expose-gc --trace-deprecation "$0" "$@"
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
 
-const parseQuery = (queryString) => {
-  let query = {}
-  queryString.split('&&').forEach((item) => {
-    let oneQuery = item.split('=');
-    if(oneQuery.length == 1) {
-      query[oneQuery[0]] = true
-    } else {
-      query[oneQuery[0]] = oneQuery[1] 
-    };
-  });
-  return query;
-};
 
-const methodHeandler = (method, cb, ...rest) => {
+const methodHandler = (method, cb, ...rest) => {
   switch (method) {
     case 'DELETE':
       cd && rest ? cb(rest) : cd && !rest ? cd() : null
@@ -30,25 +15,64 @@ const methodHeandler = (method, cb, ...rest) => {
     case 'PUT':
       cd && rest ? cb(rest) : cd && !rest ? cd() : null
       break;
-  }
-}
+  };
+};
 
-http.createServer((request, response) => {
-   const url = url.parse(request.url);
-   const METHOD = request.method;
-   const query = parseQuery(pathname.query)
-   console.log(query + " received.");
 
-   fs.readFile(pathname.pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else {	
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         response.write(data.toString());		
-      }
+
+
+const FakeServer = {
+  serverAction =[],
+  put: (path, response) => {
+    FakeServer.serverAction.push({
+      method: 'PUT',
+      path, response
+    })
+  },
+  post: (path, response) => {
+    FakeServer.serverAction.push({
+      method: 'POST',
+      path, response
+    })
+  },
+  get: (path, response) => {
+    FakeServer.serverAction.push({
+      method: 'GET',
+      path, response
+    })
+  },
+  delete: (path, response) => {
+    FakeServer.serverAction.push({
+      method: 'DELETE',
+      path, response
+    })
+  },
+  start: () => {
+    const http = require('http');
+    const fs = require('fs');
+    const url = require('url');
+    const parseQuery = (queryString) => {
+      let query = {}
+      queryString.split('&&').forEach((item) => {
+        let oneQuery = item.split('=');
+        if (oneQuery.length == 1) {
+          query[oneQuery[0]] = true
+        } else {
+          query[oneQuery[0]] = oneQuery[1]
+        };
+      });
+      return query;
+    };
+    http.createServer((request, response) => {
+      const url = url.parse(request.url);
+      const METHOD = request.method;
+      const query = parseQuery(pathname.query)
+      FakeServer.serverAction.forEach((handler))
+      // response.writeHead(404, { 'Content-Type': 'text/html' });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(data.toString());
       response.end();
-   });   
-}).listen(8085);
 
-console.log('Server running at http://127.0.0.1:8081/');
+    }).listen(8085);
+  };
+};
