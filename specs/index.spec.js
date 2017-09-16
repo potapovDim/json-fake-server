593efdcf2332b8afe7f29d529d18e4f4a5342e43
potapovDim <potapovd3012@gmail.com>
2017-09-16 11:07:18 +0300

    add tests, small fix

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,15 +1,7 @@
 const { expect } = require('chai');
-
-const FakeServer = require('../lib')
-
-
 const fetch = require("node-fetch");
+const FakeServer = require('../lib');
 
-const https = require("https");
-
-const agent = new https.Agent({
-  rejectUnauthorized: false
-})
 
 describe('FakeServer', () => {
   let server = null
@@ -18,7 +10,9 @@ describe('FakeServer', () => {
     server.get('/lol', { LOL: 'LOL' });
     server.put('/lol', { lol1: 'lol1' });
     server.post('/xxx', { LOLXXX: 'LOLXX' }, { error: 'SUPER CUSTOM ERROR' }, true, { a: 'a' });
+    server.post('/ggg', { LOLGGG: 'LOLGGG' });
     server.post('/new', { LOLNEW: 'LOLNEW' });
+    server.del('/nnn', { NNN: 'NNN' });
     server.start();
   });
   afterEach(() => {
@@ -32,12 +26,12 @@ describe('FakeServer', () => {
   it('get negative', async () => {
     {
       const result = await fetch('http://localhost:3535/aaaaa');
-      expect(result.status).to.eql(200);
+      expect(result.status).to.eql(400);
       expect(await result.json()).to.eql({ error: 'api.notfound' });
     }
     {
       const result = await fetch('http://localhost:3535');
-      expect(result.status).to.eql(200);
+      expect(result.status).to.eql(400);
       expect(await result.json()).to.eql({ error: 'api.notfound' });
     }
   });
@@ -48,16 +42,65 @@ describe('FakeServer', () => {
       expect(await result.json()).to.eql({ lol1: 'lol1' });
     }
   });
-  it('get negative', async () => {
+  it('put negative', async () => {
     {
       const result = await fetch('http://localhost:3535/aaaaa', { method: 'PUT' });
-      expect(result.status).to.eql(200);
+      expect(result.status).to.eql(400);
       expect(await result.json()).to.eql({ error: 'api.notfound' });
     }
     {
       const result = await fetch('http://localhost:3535', { method: 'PUT' });
-      expect(result.status).to.eql(200);
+      expect(result.status).to.eql(400);
       expect(await result.json()).to.eql({ error: 'api.notfound' });
     }
+  });
+  it('post ', async () => {
+    {
+      const result = await fetch('http://localhost:3535/ggg', { method: 'POST' });
+      expect(result.status).to.eql(200);
+      expect(await result.json()).to.eql({ LOLGGG: 'LOLGGG' });
+    }
+    {
+      const result = await fetch('http://localhost:3535/xxx', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ a: 'a' })
+      }); //headers["Content-Type"] = "application/json";
+      expect(result.status).to.eql(200);
+      expect(await result.json()).to.eql({ LOLXXX: 'LOLXX' });
+    }
+  });
+  it('post negative', async () => {
+    {
+      const result = await fetch('http://localhost:3535/xxx', { method: 'POST' });
+      expect(result.status).to.eql(400);
+      expect(await result.json()).to.eql({ error: 'SUPER CUSTOM ERROR' });
+    }
+    {
+      const result = await fetch('http://localhost:3535', { method: 'POST' });
+      expect(result.status).to.eql(400);
+      expect(await result.json()).to.eql({ error: 'api.notfound' });
+    }
+  });
+  it('del', async () => {
+    {
+      const result = await fetch('http://localhost:3535/nnn', { method: 'DELETE' });
+      expect(result.status).to.eql(200);
+      expect(await result.json()).to.eql({ NNN: 'NNN' });
+    }
+    {
+      const result = await fetch('http://localhost:3535/nnn', {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ test: 'test' })
+      });
+      expect(result.status).to.eql(200);
+      expect(await result.json()).to.eql({ NNN: 'NNN' });
+    }
+  });
+  it('del negative', async () => {
+    const result = await fetch('http://localhost:3535', { method: 'DELETE' });
+    expect(result.status).to.eql(400);
+    expect(await result.json()).to.eql({ error: 'api.notfound' });
   });
 });
